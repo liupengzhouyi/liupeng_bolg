cfd706b32664816564e66fae5f334abdf6395bcd
liupeng <liupeng.0@outlook.com>
2021-01-24 17:53:04 +0800

    Initial commit V 0.0.1

---
+++ src/main/java/com/liupeng/liupeng_bolg/LiupengBolgApplication.java
@@ -0,0 +1,13 @@
+package com.liupeng.liupeng_bolg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiupengBolgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiupengBolgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liupeng/liupeng_bolg/LiupengBolgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liupeng.liupeng_bolg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiupengBolgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
